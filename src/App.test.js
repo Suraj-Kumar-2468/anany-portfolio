3a1833e48fdbae61724cebd0817375daede43e0b
Suraj Kumar <60513362+Suraj-Kumar-2468@users.noreply.github.com>
2025-08-12 12:42:51 +0530

    feat: update hero section layout

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hero title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Anany Deep/i);
+  expect(titleElement).toBeInTheDocument();
 });
